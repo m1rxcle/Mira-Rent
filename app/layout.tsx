d0fdfdf86159062523430e040daebb4803f58397
Denis M1rxcle <noonebesideu@gmail.com>
2025-06-09 19:54:12 +0200

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,26 @@ import { Metadata } from "next"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadate: Metadata = {
+export const metadata: Metadata = {
 	title: "Miracle Rent",
 	description: "Сайт для покупки и аренды автомобилей.",
 	keywords: "Cars, Rent, buy car, best cars",
 	creator: "m1rxcle",
 	openGraph: {
-		images: "logo-black.png",
+		title: "Miracle Rent – аренда авто",
+		description: "Найдите свой лучший автомобиль.",
+		url: "https://mira-rent.vercel.app",
+		siteName: "Miracle Rent",
+		images: [
+			{
+				url: "https://mira-rent.vercel.app/main-logo.webp",
+				width: 1200,
+				height: 630,
+				alt: "Miracle Rent – превью",
+			},
+		],
+		locale: "ru_RU",
+		type: "website",
 	},
 }
 
