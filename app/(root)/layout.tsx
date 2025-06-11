92ee2c9f2a32a84044d3ac2e01061eaf8b2bee09
Denis M1rxcle <noonebesideu@gmail.com>
2025-06-11 21:02:57 +0200

    add footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import Footer from "@/share/components/footer"
+import { Button } from "@/share/ui"
+import { ArrowUpIcon } from "lucide-react"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -27,13 +30,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
 			<main className="min-h-screen mt-20">{children}</main>
-			<footer className="bg-blue-50 py-12  bottom-0 ">
-				<div className="container mx-auto px-4 text-center text-gray-600">
-					<p>
-						Made with <span className="animate-pulse">❤️</span> by m1rxcle.
-					</p>
-				</div>
-			</footer>
+			<Footer />
 		</>
 	)
 }
---
+++ share/components/footer.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
+const Footer = () => {
+	const [isMobile, setIsMobile] = useState(false)
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 768)
+		}
+		window.addEventListener("resize", handleResize)
+		return () => {
+			window.removeEventListener("resize", handleResize)
+		}
+	}, [isMobile])
+
+	return (
+		<footer className="bg-gradient-to-br from-blue-500 to-green-500 text-white">
+			<div className="max-w-7xl mx-auto px-4 py-12 grid grid-cols-1 md:grid-cols-4 md:gap-8 gap-4">
+				<div>
+					<h2 className="text-2xl font-bold text-white">Mira Rent</h2>
+					<p className="mt-4 text-gray-200">Продажа и аренда автомобилей премиум-класса. Надежность, стиль, комфорт — каждый день.</p>
+				</div>
+
+				<div>
+					<h3 className="text-lg font-semibold text-white mb-4">Навигация</h3>
+					<ul className="space-y-2 text-gray-300">
+						<li>
+							<Link href="/" className="hover:text-blue-300">
+								Главная
+							</Link>
+						</li>
+						<li>
+							<Link href="/cars" className="hover:text-blue-300">
+								Каталог
+							</Link>
+						</li>
+						<li>
+							<Link href="/reservations" className="hover:text-blue-300">
+								Брони
+							</Link>
+						</li>
+						<li>
+							<Link href="/saved-cars" className="hover:text-blue-300">
+								Избранные
+							</Link>
+						</li>
+					</ul>
+				</div>
+
+				<div>
+					<h3 className="text-lg font-semibold text-white mb-4">Контакты</h3>
+					<ul className="text-gray-300 space-y-2">
+						<li>г. Калининград, ул. Мира, 12</li>
+						<li>
+							<a href={isMobile ? "tel:+7 (999) 123-45-67" : ""} className="hover:text-blue-300 md:cursor-default md:pointer-events-none">
+								{isMobile ? "Позвонить" : "+7 (999) 123-45-67"}
+							</a>
+						</li>
+						<li>
+							<a href="mailto:noonebesideu@gmail.com" className="hover:text-blue-300">
+								noonebesideu@gmail.com
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div>
+					<h3 className="text-lg font-semibold text-white mb-4">Мы в соцсетях</h3>
+					<div className="flex space-x-4 text-gray-300">
+						<Link href="https://m.vk.com/noonebesidesu" className="hover:text-white">
+							<Image src={"/icons/vk1.png"} width={30} height={30} alt="vk" />
+						</Link>
+						<Link href="https://web.telegram.org/a/" className="hover:text-white">
+							<Image src={"/icons/telegram1.png"} width={30} height={30} alt="telegram" />
+						</Link>
+						<Link href="#" className="hover:text-white">
+							<Image src={"/icons/instagram1.png"} width={30} height={30} alt="instagram" />
+						</Link>
+					</div>
+				</div>
+			</div>
+
+			<div className="border-t border-white text-center text-black/70 text-sm py-4">
+				© 2025 Miracle-Rent. Все права защищены<span className="animate-ping">❤️</span>.
+			</div>
+		</footer>
+	)
+}
+export default Footer
