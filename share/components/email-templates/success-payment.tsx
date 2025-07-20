c12a923ed86fd0a8b076581e79289cabf6e87b1a
Denis M1rxcle <noonebesideu@gmail.com>
2025-07-20 21:58:56 +0200

    fix(custom): убрал jsonParse

--- share/components/email-templates/success-payment.tsx
+++ share/components/email-templates/success-payment.tsx
@@ -11,20 +11,12 @@ interface Props {
 }
 
 export function OrderSuccessTemplate({ carMake, carYear, carModel, carFuelType, carPrice, orderId }: Props) {
-	const parsedPrice = JSON.parse(carPrice.toString())
-	const parsedMake = JSON.parse(carMake)
-	const parsedYear = JSON.parse(carYear.toString())
-	const parsedModel = JSON.parse(carModel)
-	const parsedFuelType = JSON.parse(carFuelType)
-
-	console.log(parsedMake, parsedYear, parsedModel, parsedFuelType, parsedPrice)
-
 	return (
 		<div>
 			<h1>
 				Спасибо за покупку автомобиля{" "}
 				<span className="font-bold italic">
-					{parsedMake} {parsedYear} {parsedModel}
+					{carMake} {carYear} {carModel}
 				</span>
 			</h1>
 			<hr />
@@ -32,10 +24,10 @@ export function OrderSuccessTemplate({ carMake, carYear, carModel, carFuelType, 
 			<p>Детали вашего автомобиля:</p>
 			<ul>
 				<li>
-					Марка: {parsedMake}, Модель: {parsedModel}, Год: {parsedYear}, Двигатель: {parsedFuelType},
+					Марка: {carMake}, Модель: {carModel}, Год: {carYear}, Двигатель: {carFuelType},
 				</li>
 			</ul>
-			<p>Ваша покупка вышла на ${parsedPrice}.</p>
+			<p>Ваша покупка вышла на ${carPrice.toString()}.</p>
 		</div>
 	)
 }
