e071edb84661dc7b2685dcb7c279692d18b8059c
Denis M1rxcle <noonebesideu@gmail.com>
2025-07-31 18:53:43 +0200

    feat(custom): сделал тесты для ссылок в футере

--- share/test/footer.test.tsx
+++ share/test/footer.test.tsx
@@ -9,4 +9,35 @@ describe("Footer component", () => {
 
 		expect(component).toBeInTheDocument()
 	})
+
+	it("contains navigation links", () => {
+		render(<Footer />)
+
+		const homeLink = screen.getByRole("link", { name: "Главная" }) as HTMLAnchorElement
+		expect(homeLink.href).toContain("/")
+
+		const carsLink = screen.getByRole("link", { name: "Автомобили" }) as HTMLAnchorElement
+		expect(carsLink.href).toContain("/cars")
+
+		const testDrivesLink = screen.getByRole("link", { name: "Бронирование" }) as HTMLAnchorElement
+		expect(testDrivesLink.href).toContain("/reservations")
+
+		const savedCars = screen.getByRole("link", { name: "Избранные" }) as HTMLAnchorElement
+		expect(savedCars.href).toContain("/saved-cars")
+
+		const vkLink = screen.getByRole("link", { name: "vk" }) as HTMLAnchorElement
+		expect(vkLink.href).toContain("https://m.vk.com/noonebesidesu")
+
+		const telegramLink = screen.getByRole("link", { name: "telegram" }) as HTMLAnchorElement
+		expect(telegramLink.href).toContain("https://web.telegram.org/a/")
+
+		const instagramLink = screen.getByRole("link", { name: "instagram" }) as HTMLAnchorElement
+		expect(instagramLink.href).toContain("#")
+
+		const gitHubLink = screen.getByRole("link", { name: "m1rxcle" }) as HTMLAnchorElement
+		expect(gitHubLink.href).toContain("https://github.com/m1rxcle/Mira-Rent")
+
+		const mailLink = screen.getByRole("link", { name: "noonebesideu@gmail.com" }) as HTMLAnchorElement
+		expect(mailLink.href).toContain("mailto:noonebesideu@gmail.com")
+	})
 })
